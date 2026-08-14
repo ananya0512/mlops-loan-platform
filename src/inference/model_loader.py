80ad28a55ff35188be80bef0d6116aa2b5828a43
Ananya <ananyamohanty0512@gmail.com>
2026-08-14 19:14:03 +0530

    dockerize loan approval inference API

--- src/inference/model_loader.py
+++ src/inference/model_loader.py
@@ -1,5 +1,3 @@
-## This file gives the rest of your application a loaded model object.
-
 import os
 
 import mlflow
@@ -21,16 +19,87 @@ MODEL_VERSION = os.getenv(
     "1",
 )
 
+WINDOWS_MLFLOW_ROOT = os.getenv(
+    "WINDOWS_MLFLOW_ROOT",
+    "C:/Users/Ananya/Documents/projects/mlops-loan-platform/mlflow",
+)
+
+CONTAINER_MLFLOW_ROOT = os.getenv(
+    "CONTAINER_MLFLOW_ROOT",
+    "/mlflow",
+)
+
 
 def load_model():
+
     mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
-    model_uri = (
-        f"models:/{MODEL_NAME}/{MODEL_VERSION}"
+    print(
+        f"Loading model: "
+        f"{MODEL_NAME} version: {MODEL_VERSION}"
     )
 
-    model = mlflow.sklearn.load_model(                 ## MLflow retrieves the model and returns the actual trained sklearn model object.
-        model_uri
+    client = mlflow.MlflowClient()
+
+    version = client.get_model_version(
+        MODEL_NAME,
+        MODEL_VERSION,
     )
+
+    print(f"Model source: {version.source}")
+
+    artifact_uri = client.get_model_version_download_uri(
+        MODEL_NAME,
+        MODEL_VERSION,
+    )
+
+    print(f"MLflow artifact URI: {artifact_uri}")
+
+    if artifact_uri.startswith("file:"):
+
+        artifact_path = artifact_uri.replace(
+            "file:",
+            "",
+            1,
+        )
+
+        artifact_path = artifact_path.replace(
+            WINDOWS_MLFLOW_ROOT,
+            CONTAINER_MLFLOW_ROOT,
+        )
+
+        artifact_path = artifact_path.replace(
+            "\\",
+            "/",
+        )
+
+        print(
+            f"Container artifact path: "
+            f"{artifact_path}"
+        )
+
+        if not os.path.exists(artifact_path):
+
+            raise RuntimeError(
+                "MLflow artifact does not exist "
+                f"in container: {artifact_path}"
+            )
+
+        print(
+            "Loading model from "
+            "container artifact path..."
+        )
+
+        model = mlflow.sklearn.load_model(
+            artifact_path
+        )
+
+    else:
+
+        model = mlflow.sklearn.load_model(
+            f"models:/{MODEL_NAME}/{MODEL_VERSION}"
+        )
+
+    print("MODEL LOADED SUCCESSFULLY")
 
     return model
